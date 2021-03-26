c3be5fac7118d07640211a31aa6f436b6d98f388
glenbuck503 <glenbuck503@gmail.com>
2021-03-26 14:21:10 -0700

    add delete routes to treats controller

--- SweetTreats/Controllers/TreatsController.cs
+++ SweetTreats/Controllers/TreatsController.cs
@@ -14,7 +14,7 @@ namespace SweetTreats.Controllers
   [Authorize]
   public class TreatsController : Controller 
   {
-     private readonly SweetTreatsContext _db;
+    private readonly SweetTreatsContext _db;
     private readonly UserManager<ApplicationUser> _userManager;
     public TreatsController(UserManager<ApplicationUser> userManager, SweetTreatsContext db)
     {
@@ -31,7 +31,7 @@ namespace SweetTreats.Controllers
     }
 
     [Authorize]
-     public ActionResult Create()
+    public ActionResult Create()
     {
       return View();
     }
@@ -106,9 +106,30 @@ namespace SweetTreats.Controllers
       return RedirectToAction("Details", new { id = treat.TreatId});
     }
 
+    [Authorize]
+    public async Task<ActionResult> Delete(int id)
+    {
+      var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      var currentUser = await _userManager.FindByIdAsync(userId);
+      var thisTreat = _db.Treats.Where(entry => entry.User.Id == currentUser.Id).FirstOrDefault(treats => treats.TreatId == id);
+      if(thisTreat == null)
+      {
+        return RedirectToAction("Details", new { id = id});
+      }
+      return View(thisTreat);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisTreat = _db.Treats.FirstOrDefault(treats => treats.TreatId == id);
+      _db.Treats.Remove(thisTreat);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
     
 
 
-    
   }
 }
