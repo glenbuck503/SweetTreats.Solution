b0e8b06a19758666127363e65c849bb3d77f85e0
glenbuck503 <glenbuck503@gmail.com>
2021-03-26 14:38:42 -0700

    add Login and LogOut routes to Account controller. All routes linked to views

--- SweetTreats/Controllers/AccountController.cs
+++ SweetTreats/Controllers/AccountController.cs
@@ -60,5 +60,31 @@ namespace SweetTreats.Controllers
         }  
       }  
     }
+
+    public ActionResult Login()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Login(LoginViewModel model)
+    {
+      Microsoft.AspNetCore.Identity.SignInResult result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, isPersistent: true, lockoutOnFailure: false);
+      if(result.Succeeded)
+      {
+        return RedirectToAction("Index");
+      }
+      else
+      {
+        ViewBag.Error = result;
+        return View();
+      }
+    }
+    [HttpPost]
+    public async Task<ActionResult> LogOut()
+    {
+      await _signInManager.SignOutAsync();
+      return RedirectToAction("Index");
+    }
   }
 }
