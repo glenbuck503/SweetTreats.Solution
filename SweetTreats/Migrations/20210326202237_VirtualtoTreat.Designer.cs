c4feda5f5b28f532824d5d5732d05cf2f010e6f6
glenbuck503 <glenbuck503@gmail.com>
2021-03-26 13:37:48 -0700

    add index to treat view

--- SweetTreats/Migrations/20210326202237_VirtualtoTreat.Designer.cs
+++ SweetTreats/Migrations/20210326202237_VirtualtoTreat.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SweetTreats.Models;
 
 namespace SweetTreats.Migrations
 {
     [DbContext(typeof(SweetTreatsContext))]
-    partial class SweetTreatsContextModelSnapshot : ModelSnapshot
+    [Migration("20210326202237_VirtualtoTreat")]
+    partial class VirtualtoTreat
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
