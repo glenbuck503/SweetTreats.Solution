5116460dca6dd671550f7782b6328862a702face
glenbuck503 <glenbuck503@gmail.com>
2021-03-26 09:25:48 -0700

    all models and start up/program files setup. ready for first migration

---
+++ SweetTreats/Models/TreatFlavor.cs
@@ -0,0 +1,13 @@
+
+namespace SweetTreats.Models
+{
+  public class TreatFlavor
+    {       
+        public int TreatFlavorId { get; set; }
+        public int FlavorId { get; set; }
+        public int TreatId { get; set; }
+        public virtual Flavor Flavaor { get; set; }
+        public virtual Treat Treat { get; set; }
+        
+    }
+}
